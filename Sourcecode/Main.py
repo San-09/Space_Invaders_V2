e3403c8b3c7ed32aa627ba52ce5aa77dd708c64d
theep26 <santheep@hotmail.co.uk>
2020-07-26 15:17:05 +0100

    Python window
    Window created

--- Sourcecode/Main.py
+++ Sourcecode/Main.py
@@ -4,7 +4,9 @@ import os
 import time
 import random
 
-
+WIDTH, HEIGHT = 750,750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))  #Width and height are a tuple
+pygame.display.set_caption("Space Shooter Tutorial")
 
 
 #Load images from asset folder

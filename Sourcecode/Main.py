1419f91108affe5bae7f86baf5a3c688c9980c11
theep26 <santheep@hotmail.co.uk>
2020-07-26 15:43:24 +0100

    Level and lives label
    Added these values to the window

--- Sourcecode/Main.py
+++ Sourcecode/Main.py
@@ -1,8 +1,15 @@
+#Rules
+#Constants will be capatalised
+
 #import modules
 import pygame
 import os
 import time
 import random
+
+#fonts in pygame need to be initialised at the beginning of code
+pygame.font.init()
+
 
 WIDTH, HEIGHT = 750,750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))  #Width and height are a tuple
@@ -30,11 +37,21 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
     clock = pygame.time.Clock()
 
     def redraw_window():
-        WIN.blit(BG, (0,0 ))        #Draws the background screen at (0,0) at the top corner
+        WIN.blit(BG, (0,0))        #Draws the background screen at (0,0) at the top corner
+        #Draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
 
+        WIN.blit(lives_label, (10,10))
+        #The level label will be on the right edge of the screen. We want this just to the left of the right edge.
+        #The following automates this by getting the width of the label text
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
         pygame.display.update()
 
     while run:
